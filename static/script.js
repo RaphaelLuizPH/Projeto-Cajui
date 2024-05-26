eade9edf6c9483429f5f0d31d512522f37912da9
RaphaelLuizPH <130625114+RaphaelLuizPH@users.noreply.github.com>
2024-05-25 21:29:26 -0300

    Added more sections, added Typejs

--- static/script.js
+++ static/script.js
@@ -6,6 +6,24 @@ function hide() {
   }
 }
 
+// Função para o funcionamento das curtidaas
+function like() {
+  document.addEventListener("DOMContentLoaded", function () {
+    var icons = document.querySelectorAll(".heart");
+    icons.forEach(function (icon) {
+      icon.addEventListener("click", function () {
+        if (icon.getAttribute("name") === "heart-empty") {
+          icon.setAttribute("name", "heart");
+        } else {
+          icon.setAttribute("name", "heart-empty");
+        }
+      });
+    });
+  });
+}
+
+like();
+
 // Listas de primeiros nomes e sobrenomes
 const primeirosNomes = [
   "Ana",
@@ -113,6 +131,58 @@ const sobrenomes = [
   "Lopes",
 ];
 
+
+// Acionar Type.js quando o elemento entra na tela
+
+document.addEventListener("DOMContentLoaded", () => {
+  const observedElement = document.getElementById("textotweet2");
+
+  const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5, 
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        typejs();
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+
+  observer.observe(observedElement);
+});
+
+// Biblioteca Type.Js
+
+function typejs() {
+
+  var typed = new Typed("#textotweet3", {
+    strings: [
+      "                As doações que recebemos foram essenciais para recomeçar nossas vidas. Com a ajuda de todos, conseguimos roupas, alimentos que nos permitiram manter a dignidade durante esse momento tão difícil. Somos eternamente gratos.",
+    ],
+    typeSpeed: 200,
+  });
+
+  var typed = new Typed("#textotweet2", {
+    strings: [
+      "                               Perdi tudo nas enchentes, graças as doações estou conseguindo seguir forte. Cada contribuição fez uma enorme diferença, nos deu esperança e força para seguir em frente.",
+    ],
+    typeSpeed: 50,
+  });
+
+  var typed = new Typed("#textotweet1", {
+    strings: [
+      "                 Só tenho a agradecer a ajuda que vocês estão nos proporcionando.Receber roupas, cobertores e produtos de higiene nos ajudou a manter a saúde e o bem-estar da minha família.",
+    ],
+    typeSpeed: 50,
+  });
+}
+
+
+
 // Função para gerar um nome aleatório
 function Nome() {
   setInterval(function () {
@@ -130,7 +200,7 @@ function Nome() {
     document.getElementById("nomedapessoa").textContent = nomes[0];
     document.getElementById("nomedapessoa2").textContent = nomes[1];
     document.getElementById("nomedapessoa3").textContent = nomes[2];
-  }, 2000);
+  }, 1000);
 }
 
 // Código para adicionar o nome do usuário
@@ -161,8 +231,8 @@ function userdonate() {
 // Funções que fazem a página rolar até a seção selecionada
 
 function home() {
-  const about = document.getElementById("welcome");
-  about.scrollIntoView({ block: "start", behavior: "smooth" });
+  const start = document.getElementById("welcome");
+  start.scrollIntoView({ block: "start", behavior: "smooth" });
 }
 
 function about() {
@@ -171,11 +241,27 @@ function about() {
 }
 
 function heroes() {
-  const about = document.getElementById("heroes");
-  about.scrollIntoView({ block: "start", behavior: "smooth" });
+  const heroes = document.getElementById("heroes");
+  heroes.scrollIntoView({ block: "start", behavior: "smooth" });
 }
 
 function howtohelp() {
-  const about = document.getElementById("howtohelp");
-  about.scrollIntoView({ block: "start", behavior: "smooth" });
+  const help = document.getElementById("howtohelp");
+  help.scrollIntoView({ block: "start", behavior: "smooth" });
 }
+
+function depoimentos() {
+  const depoimentos = document.getElementById("depoimentos");
+  depoimentos.scrollIntoView({ block: "start", behavior: "smooth" });
+}
+
+function itens() {
+  const itens = document.getElementById("itens");
+  itens.scrollIntoView({ block: "start", behavior: "smooth" });
+}
+
+function FAQ() {
+  const itens = document.getElementById("FAQ");
+  itens.scrollIntoView({ block: "start", behavior: "smooth" });
+}
+
